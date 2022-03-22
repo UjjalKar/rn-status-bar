4e876da70fadb7d299f305001fdf94022c443592
Ujjal Kar <ujjalkar17@gmail.com>
2022-03-22 23:58:53 +0530

    🐛 fix: iOS

--- ios/RnStatusBar.m
+++ ios/RnStatusBar.m
@@ -1,5 +1,9 @@
 #import <React/RCTBridgeModule.h>
 
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 @interface RCT_EXTERN_MODULE(RnStatusBar, NSObject)
 
 RCT_EXTERN_METHOD(setStatusBarColor:(NSString *)color)
